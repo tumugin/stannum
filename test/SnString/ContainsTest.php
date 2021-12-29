92b5ace56358150e93d73d85cdc075effff14212
Kazuki Oishi <oishikazuki@gmail.com>
2021-12-30 02:25:10 +0900

    Containsのテストを書いた

--- test/SnString/ContainsTest.php
+++ test/SnString/ContainsTest.php
@@ -11,16 +11,23 @@ class ContainsTest extends TestCase
 {
     /**
      * @dataProvider providesTestStrings
+     * @return void
      */
-    public function testWithStrings(SnString $initString, SnString $containsString, bool $expected): void
+    public function testWithStrings(string $initString, string $containsString, bool $expected): void
     {
-        $this->assertSame($expected, $initString->contains($containsString));
+        $this->assertSame(
+            $expected,
+            SnString::fromString($initString)->contains(SnString::fromString($containsString))
+        );
     }
 
     public function providesTestStrings(): array
     {
         return [
-            [SnString::fromString(''), SnString::fromString(''), true],
+            ['', '', true],
+            ['藍井すず', '', true],
+            ['藍井すずだよ～', 'すず', true],
+            ['藤宮めいだよ～', 'すず', false],
         ];
     }
 }
