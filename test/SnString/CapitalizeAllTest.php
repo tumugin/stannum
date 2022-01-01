9eb83856b9907b43241a718522ceb81bdc030be8
Kazuki Oishi <oishikazuki@gmail.com>
2022-01-01 21:16:07 +0900

    テストを追加した

---
+++ test/SnString/CapitalizeAllTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tumugin\Stannum\Test\SnString;
+
+use PHPUnit\Framework\TestCase;
+use Tumugin\Stannum\SnString;
+
+class CapitalizeAllTest extends TestCase
+{
+    /**
+     * @dataProvider provideTestStrings
+     */
+    public function testWithStrings(string $testString, string $expected): void
+    {
+        $actual = SnString::fromString($testString)->capitalizeAll()->toString();
+        $this->assertSame($expected, $actual);
+    }
+
+    public function provideTestStrings(): array
+    {
+        return [
+            ['', ''],
+            ['SUZU', 'SUZU'],
+            ['suzu', 'SUZU'],
+            ['Suzu', 'SUZU'],
+            ['藍井すず(aoi suzu)', '藍井すず(AOI SUZU)'],
+        ];
+    }
+}
