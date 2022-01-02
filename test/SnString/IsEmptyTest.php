c8b0b053cdf5167c3a622433a05073a6c085a912
Kazuki Oishi <oishikazuki@gmail.com>
2022-01-02 10:37:15 +0900

    テスト追加

---
+++ test/SnString/IsEmptyTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tumugin\Stannum\Test\SnString;
+
+use PHPUnit\Framework\TestCase;
+use Tumugin\Stannum\SnString;
+
+class IsEmptyTest extends TestCase
+{
+    /**
+     * @dataProvider provideStrings
+     */
+    public function testWithStrings(string $testString, bool $expected): void
+    {
+        $this->assertSame($expected, SnString::fromString($testString)->isEmpty());
+    }
+
+    public function provideStrings(): array
+    {
+        return [
+            ['', true],
+            [' ', false],
+            ['藍井すず', false],
+        ];
+    }
+}
---
+++ test/SnString/TrimTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tumugin\Stannum\Test\SnString;
+
+use PHPUnit\Framework\TestCase;
+use Tumugin\Stannum\SnString;
+
+class TrimTest extends TestCase
+{
+    /**
+     * @dataProvider provideStrings
+     */
+    public function testWithStrings(string $testString, string $expected): void
+    {
+        $this->assertSame($expected, SnString::fromString($testString)->trim()->toString());
+    }
+
+    public function provideStrings(): array
+    {
+        return [
+            [' ', ''],
+            ['　', ''],
+            [' 藍井すず ', '藍井すず'],
+            ['藍井すず は かわいい', '藍井すず は かわいい'],
+            // String contains ZWJ sequences does not collapse
+            ['🏳️‍🌈', '🏳️‍🌈'],
+            ['虹🏳️‍🌈の コンキスタドール', '虹🏳️‍🌈の コンキスタドール'],
+        ];
+    }
+}
