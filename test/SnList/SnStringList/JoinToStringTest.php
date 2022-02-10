0010a71043b8eab3aafcd7654f02a78e8dea948a
Kazuki Oishi <oishikazuki@gmail.com>
2022-02-11 07:27:15 +0900

    Add joinToString API to SnStringList

---
+++ test/SnList/SnStringList/JoinToStringTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tumugin\Stannum\Test\SnList\SnStringList;
+
+use PHPUnit\Framework\TestCase;
+use Tumugin\Stannum\SnList\SnStringList;
+use Tumugin\Stannum\SnString;
+
+class JoinToStringTest extends TestCase
+{
+    public function testJoinToString()
+    {
+        $this->assertSame(
+            '藍井すず,藤宮めい,橋本あみ',
+            SnStringList::byStringArray([
+                '藍井すず',
+                '藤宮めい',
+                '橋本あみ',
+            ])
+                ->joinToString(SnString::byString(','))
+                ->toString()
+        );
+    }
+
+    public function testJoinToStringWithEmptyArray()
+    {
+        $this->assertSame(
+            '',
+            SnStringList::byArray([])
+                ->joinToString(SnString::byString(''))
+                ->toString()
+        );
+    }
+}
